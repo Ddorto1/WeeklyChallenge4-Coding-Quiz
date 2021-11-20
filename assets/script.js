45f8507077890ef5037601bf80b721f62c02aa53
Ddorto1 <dallasdorton@yahoo.com>
2021-11-20 09:53:31 -0700

    timer is now working with on click event listener

--- assets/script.js
+++ assets/script.js
@@ -7,10 +7,34 @@ var quizEl = document.getElementById("quiz");
 var nextEl = document.querySelector(".next");
 var countdown = 100;
 
+
+var choiceA = document.getElementById("A")
+var choiceB = document.getElementById("B")
+var choiceC = document.getElementById("C")
+var choiceD = document.getElementById("D")
+
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
+// Timer countdown function 
+function timer() {
+    var timeInterval = setInterval(function () {
 
+        if (countdown > 0) {
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            timeEl.textContent = countdown;
+            // Decrement `timeLeft` by 1
+            countdown--;
+        } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            timeEl.textContent = countdown;
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+              alert ("game over");
+        }
+    }, 1000);
+}
+startEl.addEventListener("click", timer);
 // WHEN I answer a question
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
@@ -19,3 +43,4 @@ var countdown = 100;
 // THEN the game is over
 // WHEN the game is over
 // // THEN I can save my initials and score
+
